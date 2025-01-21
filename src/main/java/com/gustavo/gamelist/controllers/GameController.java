6767a90851f159d0c2da91e5c3ea96da25555d2a
Gustavo Caçador <gustavoirl456@outlook.com>
2025-01-21 15:23:44 -0300

    GameController

---
+++ src/main/java/com/gustavo/gamelist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.gustavo.gamelist.controllers;
+
+import com.gustavo.gamelist.dto.GameDTO;
+import com.gustavo.gamelist.entities.Game;
+import com.gustavo.gamelist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameDTO> findAll() {
+        List<GameDTO> result = gameService.findAll();
+        return result;
+    }
+}
