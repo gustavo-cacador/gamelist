4fca5aba11434ae882bc03d11c8bec879f9f02ca
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-01-21 23:46:20 -0300

    domain model

---
+++ src/main/java/com/gustavo/gamelist/entities/Belonging.java
@@ -0,0 +1,54 @@
+package com.gustavo.gamelist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer posicao;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer posicao) {
+        id.setGame(game);
+        id.setList(list);
+        this.posicao = posicao;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Integer posicao) {
+        this.posicao = posicao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
