2d3f88328484f726fff53751e4bbd3000b0b6eee
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-01-22 01:24:35 -0300

    buscando os Games pelas listas

--- src/main/java/com/gustavo/gamelist/services/GameService.java
+++ src/main/java/com/gustavo/gamelist/services/GameService.java
@@ -3,6 +3,7 @@ package com.gustavo.gamelist.services;
 import com.gustavo.gamelist.dto.GameDTO;
 import com.gustavo.gamelist.dto.GameMinDTO;
 import com.gustavo.gamelist.entities.Game;
+import com.gustavo.gamelist.projections.GameMinProjection;
 import com.gustavo.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
