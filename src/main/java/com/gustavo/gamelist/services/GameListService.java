be3a30caf9bfb845ddbfc414530c46767a5541ce
Gustavo Caçador <gustavoirl456@outlook.com>
2025-01-24 14:31:59 -0300

    Adicionando move dos games nas listas

--- src/main/java/com/gustavo/gamelist/services/GameListService.java
+++ src/main/java/com/gustavo/gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.gustavo.gamelist.services;
 
 import com.gustavo.gamelist.dto.GameListDTO;
 import com.gustavo.gamelist.entities.GameList;
+import com.gustavo.gamelist.projections.GameMinProjection;
 import com.gustavo.gamelist.repositories.GameListRepository;
+import com.gustavo.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
